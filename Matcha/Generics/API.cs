5d81b1071eb9dd99963d7d0f04163a2fae01dd88
Dan Banfield <dandbanfield@gmail.com>
2022-04-19 19:10:43 +0100

    Added a base for an API structure.

---
+++ Matcha/Generics/API.cs
@@ -0,0 +1,38 @@
+﻿namespace Matcha.Generics
+{
+    public static class API
+    {
+        #region Properties
+
+        public enum AvailableAPIs { WeAreDevs, EasyExploits, Comet }
+        public static AvailableAPIs SelectedAPI = AvailableAPIs.WeAreDevs;
+
+        #endregion
+
+        public static void ExecuteScript()
+        {
+            switch (SelectedAPI)
+            {
+                case AvailableAPIs.WeAreDevs:
+                    break;
+                case AvailableAPIs.EasyExploits:
+                    break;
+                case AvailableAPIs.Comet:
+                    break;
+            }
+        }
+
+        public static void Attach()
+        {
+            switch (SelectedAPI)
+            {
+                case AvailableAPIs.WeAreDevs:
+                    break;
+                case AvailableAPIs.EasyExploits:
+                    break;
+                case AvailableAPIs.Comet:
+                    break;
+            }
+        }
+    }
+}
