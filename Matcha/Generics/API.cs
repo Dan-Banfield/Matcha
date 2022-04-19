8fec52fefc540e776d285624b81c5f7ebb65ba22
Dan Banfield <dandbanfield@gmail.com>
2022-04-19 19:55:45 +0100

    Updated the API structure.

--- Matcha/Generics/API.cs
+++ Matcha/Generics/API.cs
@@ -7,7 +7,39 @@ namespace Matcha.Generics
         #region Properties
 
         public enum AvailableAPIs { WeAreDevs, EasyExploits, Comet }
-        public static AvailableAPIs SelectedAPI = AvailableAPIs.WeAreDevs;
+
+        public static AvailableAPIs SelectedAPI
+        {
+            get
+            {
+                switch (Properties.Settings.Default.selectedAPI)
+                {
+                    case 0:
+                        return AvailableAPIs.WeAreDevs;
+                    case 1:
+                        return AvailableAPIs.EasyExploits;
+                    case 2:
+                        return AvailableAPIs.Comet;
+                }
+                return AvailableAPIs.WeAreDevs;
+            }
+            set
+            {
+                switch (value)
+                {
+                    case AvailableAPIs.WeAreDevs:
+                        Properties.Settings.Default.selectedAPI = 0;
+                        break;
+                    case AvailableAPIs.EasyExploits:
+                        Properties.Settings.Default.selectedAPI = 1;
+                        break;
+                    case AvailableAPIs.Comet:
+                        Properties.Settings.Default.selectedAPI = 2;
+                        break;
+                }
+                Properties.Settings.Default.Save();
+            }
+        }
 
         private static ExploitAPI weAreDevsAPI = new ExploitAPI();
 
